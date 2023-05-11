dfb71d157cf68c045f4acf1556003b3c51d9837f
Musong <ernestakeh@gmail.com>
2023-05-11 17:10:50 +0100

    Add filter method

--- enumerable.rb
+++ enumerable.rb
@@ -14,9 +14,13 @@ class MyList
   def less_than_5
    puts(@list.all? { |item| item < 5 })
   end
+  def filter_even_numbers
+   puts(@list.filter { |item| item.even? })
+  end
 end
 
 list = MyList.new([1,2,3,4])
 
 list.less_than_5
+list.filter_even_numbers
   
