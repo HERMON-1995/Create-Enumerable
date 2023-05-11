c790b650cc706a3a7fcadbaff4d8dd525f078d37
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-05-11 14:26:40 +0100

    Add class named MyList with an instance variable named List, add a method in module named less_than_5

--- enumerable.rb
+++ enumerable.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
 # My Enumerable module is top_level module for three method
-module MyEnumerable
-  def filter
-    new_arr = []
-    each { |x| new_arr << x if yield(x) }
-    new_arr
+class MyList
+  attr_accessor :list
+  def initialize
+    @list = [1,2,3,4]
   end
+ module MyEnumerable
+     def filter
+        new_arr = []
+        each { |x| new_arr << x if yield(x) }
+        new_arr
+    end
+  end
+  def less_than_5
+    @list.all? { |item| item < 5 }
+ end
+ include MyList
 end
+  
